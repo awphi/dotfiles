44dbecd34f4115ad6e365238a2329d6f3e343e07
awphi <26072111+awphi@users.noreply.github.com>
2025-10-24 22:53:48 +0100

    apps

--- run_onchange_install_packages.sh
+++ run_onchange_install_packages.sh
@@ -2,15 +2,22 @@
 
 # CLI tools
 brew install ripgrep
-brew install --cask codex
 brew install colima
 brew install zsh-autosuggestions
 brew install gh
 
+# Applications
+brew install --cask codex
+brew install --cask zed
+brew install --cask google-chrome
+brew install --cask spotify
+
+# Fonts
+brew install --cask font-hack-nerd-font
+
 # Mise
 brew install mise
 mise install
-
 # https://mise.jdx.dev/configuration.html#idiomatic-version-files
 mise settings add idiomatic_version_file_enable_tools python
 mise settings add idiomatic_version_file_enable_tools node
@@ -18,9 +25,3 @@ mise settings add idiomatic_version_file_enable_tools go
 
 # Terminal prompt
 brew install starship
-
-# Editors
-brew install --cask zed
-
-# Fonts
-brew install --cask font-hack-nerd-font
